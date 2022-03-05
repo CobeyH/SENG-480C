528d240de4d6e5339367b970f56bd5da342ec33c
Cobey Hollier <cobeyhollier@Cobeys-MacBook-Pro.local>
2022-03-05 15:40:59 -0800

    CH: Adding magnitude checking

--- A2/SENG480C-A2.ino
+++ A2/SENG480C-A2.ino
@@ -5,7 +5,7 @@
 const int stepsPerRevolution = 2048;
 
 int analogPin = A5;
-int raw = 0;
+int resVal = 0;
 int Vin = 5;
 float Vout = 0;
 float R1 = 2000;
@@ -20,28 +20,65 @@ float buffer = 0;
 Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
 
 void setup() {
-  // Set the speed to 15 rpm:
-  myStepper.setSpeed(5);
+  myStepper.setSpeed(1);
   
   // Begin Serial communication at a baud rate of 9600:
   Serial.begin(9600);
 }
 
-void loop() {
-  myStepper.step(stepsPerRevolution);
-  delay(50);
-
+void loop() {  
+    Serial.println("Loop");
   // Ohm Meter Code:
-   raw = analogRead(analogPin);
-  if(raw){
-    buffer = raw * Vin;
+   resVal = analogRead(analogPin);
+  if(resVal){
+    buffer = resVal * Vin;
     Vout = (buffer)/1024.0;
     buffer = (Vin/Vout) - 1;
     R2= R1 * buffer;
-    Serial.print("Vout: ");
-    Serial.println(Vout);
-    Serial.print("R2: ");
+    int magnitudeIndex = getEnergyIndex();
+    Serial.print("Magnitude: ");
+    Serial.println(5 + 0.1 * magnitudeIndex);
+    // Set motor speed
+    Serial.print("Resistance: ");
     Serial.println(R2);
-    delay(1000);
+  } else {
+    myStepper.setSpeed(1);
+  }
+  delay(1000);
+}
+
+int getEnergyIndex() {
+  if(R2 <= 28.5) {
+    return 0;
+  } 
+  if(R2 <= 73.5) {
+    return 1;
+  }
+  if(R2 <= 160) {
+    return 2;
+  }
+  if(R2 <= 275) {
+    return 3;
+  }
+  if(R2 <= 505) {
+    return 4;
+  }
+  if(R2 <= 840) {
+    return 5;
+  }
+  if(R2 <= 1500) {
+    return 6;
+  }
+  if(R2 <= 3500) {
+    return 7;
+  }
+  if(R2 <= 7500) {
+    return 8;
+  }
+  if(R2 <= 50000) {
+    return 9;
+  }
+  if(R2 <= 200000) {
+    return 10;
   }
 }
